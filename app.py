f51bbabb88500a63f3843a5b5224be494f4fa1c6
Ayman <enkhalifapro@gmail.com>
2023-10-31 21:48:28 +0200

    add timestamp to info and error logs

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.DEBUG,
     datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.ERROR,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+
 logger = logging.getLogger('app')
 logger.addHandler(stdout_hdlr)
 logger.addHandler(stderr_hdlr)
